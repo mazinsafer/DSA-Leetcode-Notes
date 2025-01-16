d422b60c265a503a7274f862a291a78c8da0eb01
Mazin Safer <mazinsafer10@gmail.com>
2025-01-16 17:08:13 -0600

    MajorityElement

---
+++ MajorityElement.py
@@ -0,0 +1,15 @@
+def majorityElement(self, nums):
+        candidate = None
+        counter = 0
+        n = len(nums)
+        for i in nums:
+            if counter == 0:
+                candidate = i
+            if candidate != i:
+                counter = counter - 1
+            if candidate == i:
+                counter = counter + 1
+        return candidate
+
+
+    
