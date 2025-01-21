b3e9b913fa9c21d439b9815749a55135745e4fe0
Mazin Safer <mazinsafer10@gmail.com>
2025-01-20 18:44:44 -0600

    Strings and Arrays

---
+++ LinkedlistNotes.py
@@ -0,0 +1,21 @@
+## Linked list notes (single and double linked)
+
+## Singlly linked list have only 1 connection between nodes
+
+## last item in list is None/Null
+
+## class create objects, creating the node
+
+## node.next= ref to a diff object
+
+## dont have indexes
+
+## add, lookup, delete, and access a node are all O(n)
+
+## removing and adding sum at the beginning is O(1)
+
+## For doubly linked lists, they have pointers for both regular and inverse direction
+# They will have node.val, node.next, node.prev
+
+
+
