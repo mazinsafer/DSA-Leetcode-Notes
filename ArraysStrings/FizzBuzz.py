e720fae910fcdcef0e5d0f8536893163bb8d246c
Mazin Safer <mazinsafer10@gmail.com>
2025-02-07 16:25:25 -0600

    Arrays and Strings

---
+++ ArraysStrings/FizzBuzz.py
@@ -0,0 +1,19 @@
+def FizzBuzz():
+    for i in range(101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        if i % 3 == 0:
+            print("Fizz")
+        if i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+FizzBuzz()
+    
+
+     
+    
+
+    
+      
+    
---
+++ ArraysStrings/Palindrome.py
@@ -0,0 +1,26 @@
+
+    ## left pointer will be left =0, right pointer will be length(str) - 1
+    ## skip nonalphanumeric characters
+    ## check if first character is the same as the last,
+    ## if not return False
+    ## if it is the same move left pointer to the right, and right pointer to the left
+    ## iterate left and right
+    ## do the same (check if the characters are the same)
+
+def is_palindrome(s):
+   left = 0
+   right = len(s) - 1
+   while right > left: 
+        if not s[left].isalnum():
+            left +=1
+            continue
+        if not s[right].isalnum():
+            right -=1
+            continue
+        if s[left].lower() != s[right].lower():
+            return False
+        left +=1
+        right -=1
+   return True
+
+is_palindrome()
