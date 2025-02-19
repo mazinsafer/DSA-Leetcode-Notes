71f77800d5c814f3fb678d9fd7c3221b39a4235f
Mazin Safer <mazinsafer10@gmail.com>
2025-02-18 21:56:58 -0600

    Leetcode #83

--- LinkedLists/DeleteDuplicates.py
+++ LinkedLists/DeleteDuplicates.py
@@ -1,3 +1,9 @@
+## check if we have curr and cur.next because if we dont have cur.next (cur.next = None) theres no chance of duplicate
+## if the current node is equal to curr.next, we want to skip it by setting curr = cur.next.next
+## now that you have deleted duplicates for the curr node you can safely assign curr = curr.next
+## then return the head
+
+
 class Solution(object):
     def deleteDuplicates(self, head):
         cur = head
