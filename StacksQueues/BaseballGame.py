a43b244cba6c908818403ee2aedc771b7fbb8b29
Mazin Safer <mazinsafer10@gmail.com>
2025-03-07 17:46:37 -0600

    Leetcode #682

--- StacksQueues/BaseballGame.py
+++ StacksQueues/BaseballGame.py
@@ -1,14 +1,13 @@
 def calPoints(self, operations):
-        record = []
-        output = 0
-        for operation in operations:
+        record = [] ## list to keep track of opeations
+        for operation in operations: ## iterate through operations
             if operation == "+":
-                record.append(int(record[-1] + (record[-2])))
+                record.append(int(record[-1] + (record[-2]))) ## add sum of last two scores
             if operation == "C":
-                record.pop()
+                record.pop() ## remove last score
             if operation == "D":
-                record.append(int(2*(record[-1])))
+                record.append(int(2*(record[-1])))   ## add double last score
             elif operation not in ("+", "C", "D"):
                 record.append(int(operation))
-        return sum(record)
+        return sum(record) # return the sum of all the scores
 
